a2a2cc9bade122b2565369125f79645881d81d6a
GomeAvi <128034125+GomeAvi@users.noreply.github.com>
2025-06-12 20:15:41 +0300

    Shorter videos

--- video_display.py
+++ video_display.py
@@ -184,5 +184,5 @@ def play_video(path):
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    video_path = r".\\hyperFocus\\assets\\How to Speak So That People Want to Listen _ Julian Treasure _ TED.mp4"
+    video_path = r".\\hyperFocus\\assets\\How to Speak So That People Want to Listen _ Julian Treasure _ TED_short.mp4"
     play_video(video_path)
